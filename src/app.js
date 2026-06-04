5f6d5ca325677d58922b253fe1bf0bc57239d234
hogu-giriboy <choo6078@kakao.com>
2026-06-04 15:08:34 +0900

    chore: Swagger 문서 환경 구성

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import authRouter from './routes/auth.route.js';
+import swaggerUi from 'swagger-ui-express';
+import { swaggerSpec } from './docs/swagger.js';
 
 const app = express();
 
@@ -8,5 +10,11 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRouter);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// test route
+app.get('/', (req, res) => {
+  res.send('API Server Running 🚀');
+});
 
 export default app;
